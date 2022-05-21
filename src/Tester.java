40a64462f68c70d3c1e417091b03a8cf3328a6c6
mohamed <mohamed.baioumy1@outlook.com>
2022-05-21 16:50:50 -0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/Book.java

--- src/Tester.java
+++ src/Tester.java
@@ -4,5 +4,6 @@ public class Tester {
     public static void main(String[] args) {
         System.out.println("testing");
         List<Book> bookList = BookDatabaseManager.getAllBooks();
+        bookList.forEach(book -> book.printBookInfo(System.out));
     }
 }
